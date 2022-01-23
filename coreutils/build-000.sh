2e64efa4ddc200a361c65b3d8b222c9c3a7e3308
Joël Krähemann <jkraehemann@gmail.com>
2022-01-23 13:58:15 +0000

    bash build and coreutils build script

---
+++ coreutils/build-000.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $LFS/usr/src/build
+
+tar -xxf $LFS/usr/src/sources/coreutils-8.32.tar.xz
+
+cd coreutils-8.32
+
+./configure --prefix=/usr                     \
+            --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess) \
+            --enable-install-program=hostname \
+            --enable-no-install-program=kill,uptime
+
+make
+make DESTDIR=$LFS install
+
+mv -v $LFS/usr/bin/chroot                                     $LFS/usr/sbin
+mkdir -pv $LFS/usr/share/man/man8
+mv -v $LFS/usr/share/man/man1/chroot.1                        $LFS/usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/'                                           $LFS/usr/share/man/man8/chroot.8
+
