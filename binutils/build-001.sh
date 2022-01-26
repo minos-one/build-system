99256db0e926d54cf0795cba5e25a7b6cbbc9f13
Joël Krähemann <jkraehemann@gmail.com>
2022-01-26 05:53:16 +0000

    xz build and binutils 001 build script

---
+++ binutils/build-001.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd $LFS/usr/src/build
+
+tar -xxf $LFS/usr/src/sources/binutils-2.37.tar.xz
+
+cd binutils-2.37
+
+mkdir -v build
+cd       build
+
+../configure                   \
+    --prefix=/usr              \
+    --build=$(../config.guess) \
+    --host=$LFS_TGT            \
+    --disable-nls              \
+    --enable-shared            \
+    --disable-werror           \
+    --enable-64-bit-bfd
+
+
+make
+make DESTDIR=$LFS install -j1
+install -vm755 libctf/.libs/libctf.so.0.0.0 $LFS/usr/lib
