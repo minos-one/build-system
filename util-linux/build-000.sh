f68aa4af11e3eaff4d5c3ace38e8200f10d42cee
Joël Krähemann <jkraehemann@gmail.com>
2022-01-30 20:43:37 +0000

    texinfo 000 build and util-linux build script

---
+++ util-linux/build-000.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $LFS/usr/src/build
+
+tar -xxf $LFS/usr/src/sources/util-linux-2.37.2.tar.xz
+
+cd util-linux-2.37.2
+
+mkdir -pv /var/lib/hwclock
+
+./configure ADJTIME_PATH=/var/lib/hwclock/adjtime    \
+            --libdir=/usr/lib    \
+            --docdir=/usr/share/doc/util-linux-2.37.2 \
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-pylibmount \
+            --disable-static     \
+            --without-python     \
+            runstatedir=/run
+
+make
+make install
